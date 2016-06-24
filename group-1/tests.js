eedef1b755c62f1de2915a709445aa674aa2f8f0
Wekoslav Stefanovski <swekster@gmail.com>
2016-06-24 20:05:03 +0200

    Added basic tests

--- group-1/tests.js
+++ group-1/tests.js
@@ -1,7 +1,25 @@
-QUnit.test( "testing type coercion of integer to string", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test("testing valid object with valid age", function(assert){
+    //arrange
+    var person = {name : "Wekoslav", age:20};
+    var age = 0x26;
+
+    //act
+    var result = setAge(person, age);
+
+    //assert
+    assert.equal(person.age, age);
+    assert.ok(result);
 });
 
-QUnit.test( "testing integer and string comparisson without type coersion", function( assert ) {
-  assert.ok( 1 === "1", "Passed!" );
+QUnit.test("testing valid object with negative age", function(assert){
+    //arrange
+    var person = {name : "Wekoslav", age:20};
+    var age = -20;
+
+    //act
+    var result = setAge(person, age);
+
+    //assert
+    assert.equal(person.age, 20);
+    assert.notOk(result);
 });
