4b63131f4d02878d53781cc5b0944bf90a55a0c5
Wekoslav Stefanovski <swekster@gmail.com>
2016-06-25 16:08:10 +0200

    Added VS UT Solution

---
+++ group-1/UnitTestsDemo/Entities/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Person
+    {
+        public Person (string firstName, string lastName, int age)
+        {
+            if (string.IsNullOrEmpty(firstName))
+                throw new ArgumentException("First name cannot be empty");
+
+            if(age < 0 || age >= 100)
+                throw new ArgumentOutOfRangeException("Age is not in the valid range");
+
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public int Age { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+    }
+}
---
+++ group-1/UnitTestsDemo/EntitiesTests/PersonTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entities;
+
+namespace EntitiesTests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void Constructor_ValidValues_Valid_Test()
+        {
+            //arrange
+            var firstName = "Wekoslav";
+            var lastName = "Stefanovski";
+            var age = 38;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+
+            //assert
+            Assert.AreEqual(firstName, weko.FirstName);
+            Assert.AreEqual(lastName, weko.LastName);
+            Assert.AreEqual(age, weko.Age);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_InvalidFirstName_EmptyString_Exception_Test()
+        {
+            //arrange
+            var firstName = string.Empty;
+            var lastName = "Stefanovski";
+            var age = 38;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
+        public void Constructor_InvalidFirstName_Null_Exception_Test()
+        {
+            //arrange
+            string firstName = null;
+            var lastName = "Stefanovski";
+            var age = 38;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
+        public void Constructor_NegativeAge_Exception_Test()
+        {
+            //arrange
+            var firstName = "Wekoslav";
+            var lastName = "Stefanovski";
+            var age = -45;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
+        public void Constructor_OverflowAge_Exception_Test()
+        {
+            //arrange
+            var firstName = "Wekoslav";
+            var lastName = "Stefanovski";
+            var age = 450;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+        }
+
+        [TestMethod]
+        public void Constructor_ZeroAge_Valid_Test()
+        {
+            //arrange
+            var firstName = "Wekoslav";
+            var lastName = "Stefanovski";
+            var age = 0;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+
+            //assert
+            Assert.AreEqual(firstName, weko.FirstName);
+            Assert.AreEqual(lastName, weko.LastName);
+            Assert.AreEqual(age, weko.Age);
+        }
+
+        [TestMethod]
+        public void Constructor_NinetyNineAge_Valid_Test()
+        {
+            //arrange
+            var firstName = "Wekoslav";
+            var lastName = "Stefanovski";
+            var age = 99;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+
+            //assert
+            Assert.AreEqual(firstName, weko.FirstName);
+            Assert.AreEqual(lastName, weko.LastName);
+            Assert.AreEqual(age, weko.Age);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
+        public void Constructor_HundredAge_Exception_Test()
+        {
+            //arrange
+            var firstName = "Wekoslav";
+            var lastName = "Stefanovski";
+            var age = 100;
+
+            //act
+            var weko = new Person(firstName, lastName, age);
+        }
+
+
+
+    }
+}
